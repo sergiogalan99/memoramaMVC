61215d4f53ce2b721c9ff18ba3a0c273926aa945
AnaC <AnaC@192.168.1.206>
2018-03-21 13:51:20 +0100

    inicial casilla

--- src/modelo/Casilla.java
+++ src/modelo/Casilla.java
@@ -1,5 +1,23 @@
 package modelo;
 
 public class Casilla {
+	private boolean velada;
+	private int valor;
+
+	public boolean isEstado() {
+		return velada;
+	}
+
+	public void setEstado(boolean velada) {
+		this.velada = velada;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
 
 }
