be03e38b8357b68c05ac0f5c188106a5a2ad6bf2
sergiogalan99 <sergiocalamonte2018@gmail.com>
2018-04-11 18:04:27 +0200

    ComprobableTest

---
+++ src/test/ComprobableTest.java
@@ -0,0 +1,71 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+import control.Comprobable;
+import modelo.Carta;
+import modelo.Tablero;
+
+class ComprobableTest {
+
+	Comprobable instancia = new Tablero(4);
+
+	@Ignore
+	void testComprobarParejas() {
+		Carta[][][] cartas = { { { new Carta(), new Carta() }, { new Carta(), new Carta() }},
+				{{ new Carta(), new Carta() }, { new Carta(), new Carta() } } };
+		for (int i = 0; i < cartas.length; i++) {
+			for (int j = 0; j < cartas.length; j++) {
+				for (int j2 = 0; j2 < cartas.length; j2++) {
+					cartas[i][j][j2].setValor(j2);
+				}
+
+			}
+		}
+		cartas[0][0][0].setMarcada(true);
+		cartas[0][1][0].setMarcada(true);
+
+		cartas[1][0][0].setMarcada(true);
+		cartas[1][0][1].setMarcada(true);
+
+		assertTrue(this.instancia.comprobarParejas(cartas[0]));
+		assertFalse(this.instancia.comprobarParejas(cartas[1]));
+	}
+
+	@Test
+	void testComprobarMarcable() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	void testComprobarCompletado() {
+		Carta[][][] cartas = { { { new Carta(), new Carta() }, { new Carta(), new Carta() }},
+				{{ new Carta(), new Carta() }, { new Carta(), new Carta() } } };
+		for (int i = 0; i < cartas.length; i++) {
+			for (int j = 0; j < cartas.length; j++) {
+				for (int j2 = 0; j2 < cartas.length; j2++) {
+					cartas[i][j][j2].setValor(j2);
+				}
+
+			}
+		}
+		cartas[0][0][0].setMarcada(true);
+		cartas[0][1][0].setMarcada(true);
+
+		cartas[1][0][0].setMarcada(true);
+		cartas[1][0][1].setMarcada(true);
+
+		assertTrue(this.instancia.comprobarCompletado(cartas[0]));
+		assertFalse(this.instancia.comprobarCompletado(cartas[1]));
+	
+	}
+
+	@Ignore
+	void testComprobarDerrota() {
+	}
+	
+
+}
